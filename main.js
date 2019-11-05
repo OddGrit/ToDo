b23f7c7292fc1571a55102931268afdfa4f274a4
pellelito <pelle.lofstrand@gmail.com>
2019-11-05 08:54:16 +0100

    Add files via upload
    Har fått till så att listan läses in från en array, samt uppdaterar denna när de tas bort :-)
    Så nu är vi på banan för G i alla fall.
    Ska jag kika på att flytta ordningen, och du kikar på storlek och ev färgändringen till i morgon?

--- main.js
+++ main.js
@@ -17,10 +17,14 @@ function addItem()
 {
     var text = document.getElementById("input").value;
     $("#list").append('<li onclick="itemClicked(this)" class="listItem">' + text + '</li>');
+    items.push(text);
+    document.getElementById("input").value = "";
+
 }
 
 function removeItems()
 {
+     // alert(items);
 //    var itemList = document.getElementsByClassName("listItem");
 //    for (i = 0; i < itemList.length; ++i){
 //        if (itemList[n].hasClass('clicked')){
@@ -30,7 +34,26 @@ function removeItems()
 //    }
     $("#list").children('li').each(function(){
         
-        if (this.classList.contains('clicked'))
+        if (this.classList.contains('clicked')){
+            removeItem =  $(this).text();
+             items.splice( $.inArray(removeItem, items), 1 );
             $(this).remove(); 
+        }
+         
     });
+    
 }
+
+//___________________________________________________________________________--
+//Pelle kod
+
+var items = ["äta","sova","handla"];
+var removeItem =""
+
+$( document ).ready(function() {
+   $('#list li').each(function (i) {
+    $(this).append(items[i]);
+});
+});
+
+//______________________________________________________________________________-
