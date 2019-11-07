ae7e36a03c6077e8ea55c7801766618d95567c0f
pellelito <pelle.lofstrand@gmail.com>
2019-11-07 12:00:17 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var clicked = false;
+var items = ["äta","sova","handla"];
+var removeItem =""
 
-function itemClicked(itemClicked){
+function itemClicked(itemClicked){                      //Kollar om element är markerat, annars markerar
     var item = itemClicked;
     
-    //alert(document.getElementById("l1").textContent);
     if (clicked) {
         itemClicked.classList.remove('clicked');
         clicked = !clicked;
@@ -16,53 +17,44 @@ function itemClicked(itemClicked){
 function addItem()
 {
     var text = document.getElementById("input").value;
-    $("#list").append('<li onclick="itemClicked(this)" class="listItem">' + text + '</li>');
-    items.push(text);
+    $("#list").append('<li onclick="itemClicked(this)" class="listItem">' + text + '</li>');        //Lägger till nytt element i listan
+    items.push(text);                                                                                                                             // lägger till nytt element i array 
     document.getElementById("input").value = "";
-
+    save();
 }
 
 function removeItems()
 {
-     // alert(items);
-//    var itemList = document.getElementsByClassName("listItem");
-//    for (i = 0; i < itemList.length; ++i){
-//        if (itemList[n].hasClass('clicked')){
-//            alert("boo");
-//            itemList[n].removeChild();
-//        }
-//    }
     $("#list").children('li').each(function(){
         
         if (this.classList.contains('clicked')){
             removeItem =  $(this).text();
              items.splice( $.inArray(removeItem, items), 1 );
             $(this).remove(); 
+            save();
         }
          
     });
 }
-$(document).ready(function(){
+$(document).ready(function(){                           // Gör knappar större
     $("button").hover(function(){
         $(this).animate({padding: "2px 20px"}, 1000);
     }, function(){
         $(this).animate({padding: "2px 10px"}, 1000);
     });
 });
-//___________________________________________________________________________--
-//Pelle kod
 
-var items = ["äta","sova","handla"];
-var removeItem =""
-
-$( document ).ready(function() {
-   $('#list li').each(function (i) {
-    $(this).append(items[i]);
-});
+ $( document ).ready(function() {                               //Läser in ev sparad lista sedan tidigare
+    items =  JSON.parse(localStorage.getItem("array"));
+     items.forEach(function(el){
+        $("#list").append('<li onclick="itemClicked(this)" class="listItem">'+el+'</li>');
+    } );
 });
     
-$(function  () {
+$(function  () {                                    //Gör listan Sorterbar
   $("#list").sortable();
 });
 
-//______________________________________________________________________________-
+function save(){                                // sparar listan i locastorage
+localStorage.setItem("array", JSON.stringify(items));
+};
