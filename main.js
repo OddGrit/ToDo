e50e447acb7297ed34beb5f1b8a24c9a8d166ea1
pellelito <pelle.lofstrand@gmail.com>
2019-11-09 13:17:38 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
+/*eslint-env browser*/
+/*global $, jQuery, alert*/
+
 var clicked = false;
 var items = [""];
-var removeItem =""
+var removeItem = "";
 
-function itemClicked(itemClicked){                      //Kollar om element är markerat, annars markerar
+function save() {
+    "use strict";   // sparar listan i locastorage
+    localStorage.setItem("array", JSON.stringify(items));
+}
+
+function itemClicked(itemClicked) {                      //Kollar om element är markerat, annars markerar
+    "use strict";
     var item = itemClicked;
     
     if (clicked) {
@@ -14,70 +23,68 @@ function itemClicked(itemClicked){                      //Kollar om element är 
     }
 }
 
-function addItem()
-{
-
-   $( "li" ).each(function( index ) {                                                                                                      //Kollar efter dubbletter
-             if ($( this ).text()==document.getElementById("input").value) {
-               alert('Du håller på att lägga till en dublett!');
-               document.getElementById("input").value ="";
-               return false;
-             } 
-               }); 
-    if (document.getElementById("input").value<1) {                                                                    //Kollar att man fyller i någonting
-       return false;
-    }   else{
-
-          var text = document.getElementById("input").value;
-            $("#list").append('<li onclick="itemClicked(this)" class="listItem">' + text + '</li>');        //Lägger till nytt element i listan
-            items.push(text);                                                                                                                             // lägger till nytt element i array 
+function addItem() {
+    "use strict";
+    $("li").each(function (index) {                                                                                                      //Kollar efter dubbletter
+        if ($(this).text() === document.getElementById("input").value) {
+            alert('Du håller på att lägga till en dublett!');
             document.getElementById("input").value = "";
-            document.getElementById("input").focus();
-            save(); 
+            return false;
+        }
+    });
+    if (document.getElementById("input").value < 1) {             //Kollar att man fyller i någonting
+        return false;
+    } else {
 
-      }
+        var text = document.getElementById("input").value;
+        $("#list").append('<li onclick="itemClicked(this)" class="listItem">' + text + '</li>');        //Lägger till nytt element i listan
+        items.push(text);                                                                                                                             // lägger till nytt element i array 
+        document.getElementById("input").value = "";
+        document.getElementById("input").focus();
+        save();
     }
+}
 
-function removeItems()                                  //plockar bort ifrån listan
-{
-    $("#list").children('li').each(function(){
+function removeItems() {                                  //plockar bort ifrån listan
+    "use strict";
+    $("#list").children('li').each(function () {
         
-        if (this.classList.contains('clicked')){
+        if (this.classList.contains('clicked')) {
             removeItem =  $(this).text();
-             items.splice( $.inArray(removeItem, items), 1 );               //Plockar bort ifrån array
-            $(this).remove();  
-            document.getElementById("input").focus();                                                                     //plockar bort ifrån själva listan
+            items.splice($.inArray(removeItem, items), 1);               //Plockar bort ifrån array
+            $(this).remove();
+            document.getElementById("input").focus();                    //plockar bort ifrån själva listan
             save();
         }
          
     });
 }
-$(document).ready(function(){                           // Gör knappar större
-    $("button").hover(function(){
+$(document).ready(function () {                           // Gör knappar större
+    "use strict";
+    $("button").hover(function () {
         $(this).animate({padding: "2px 20px"}, 1000);
-    }, function(){
+    }, function () {
         $(this).animate({padding: "2px 10px"}, 1000);
     });
 });
 
- $( document ).ready(function() {                               //Läser in ev sparad lista sedan tidigare
+$(document).ready(function () {                               //Läser in ev sparad lista sedan tidigare
+    "use strict";
     items =  JSON.parse(localStorage.getItem("array"));
-     items.forEach(function(el){
-        $("#list").append('<li onclick="itemClicked(this)" class="listItem">'+el+'</li>');
-    } );
-     document.getElementById("input").focus();
+    items.forEach(function (el) {
+        $("#list").append('<li onclick="itemClicked(this)" class="listItem">' + el + '</li>');
+    });
+    document.getElementById("input").focus();
 });
     
-$(function  () {                                    //Gör listan Sorterbar
-  $("#list").sortable();
+$(function () {                                    //Gör listan Sorterbar
+    "use strict";
+    $("#list").sortable();
 });
-
-function save(){                                // sparar listan i locastorage
-localStorage.setItem("array", JSON.stringify(items));
-};
-
- $(document).on('keypress',function(e) {                                                    //Lyssnar efter enter
-    if(e.which == 13) {
+    
+$(document).on('keypress', function (e) {                                                    //Lyssnar efter enter
+    "use strict";
+    if (e.which === 13) {
         addItem();
     }
 });
